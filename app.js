c208a4e6fa62ba89caf305a34f4252b6212a09a7
ChidponJaimun <chidpon.j@gmail.com>
2021-11-11 01:33:01 +0700

    0

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'; // dotenv import
-// const ejs from "ejs"
+import ejs from "ejs"
 // const _ from "lodash"
 // const React from "react"
 import React from "react";
@@ -7,10 +7,20 @@ import express from "express";
 import mongoose from "mongoose";
 
 
+
 const app = express();
 dotenv.config();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+// body parser
+//ejs
+app.set("view engine", "ejs");
+//ejs
+
 const password = process.env.DB_PASSWORD;
-console.log(password);
 
 mongoose.connect("mongodb+srv://joe:"+password+"@goku.pna51.mongodb.net/checklist?retryWrites=true&w=majority", {
   useNewUrlParser: true
@@ -21,20 +31,22 @@ const userSchema = new mongoose.Schema({ // a part of encrypt added at new mongo
   username: String,
   password: String,
 });
+
+
 const User = new mongoose.model("User", userSchema);
 
 
 app.get("/",(req,res)=>{
-
-  User.findOne({"username":"admin"}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        console.log(foundUser)
-      }
-    }
-  });
+ res.render("login");
+  // User.findOne({"username":"admin"}, function(err, foundUser) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       console.log(foundUser)
+  //     }
+  //   }
+  // });
 
 })
 
