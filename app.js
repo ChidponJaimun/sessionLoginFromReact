48153b04c0490c5003d95f42dadf5290571a1f73
ChidponJaimun <chidpon.j@gmail.com>
2021-11-10 21:56:31 +0700

    “2”

---
+++ app.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'; // dotenv import
+// const ejs from "ejs"
+// const _ from "lodash"
+// const React from "react"
+import React from "react";
+import express from "express";
+import mongoose from "mongoose";
+
+
+const app = express();
+const password = process.env.dbpassword;
+dotenv.config();
+
+mongoose.connect("mongodb+srv://joe:KHB2tPH25DNRWimO@goku.pna51.mongodb.net/checklist?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
+
+
+const userSchema = new mongoose.Schema({ // a part of encrypt added at new mongoose.Schema
+  username: String,
+  password: String,
+});
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/",(req,res)=>{
+
+  User.findOne({"username":"admin"}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        console.log(foundUser)
+      }
+    }
+  });
+
+})
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
