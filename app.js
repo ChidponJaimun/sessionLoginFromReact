9b6087f5fa19bb22ee23387088a1bd802d82340e
ChidponJaimun <chidpon.j@gmail.com>
2021-11-13 17:00:11 +0700

    1

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(cookieParser());
+app.use(cookieParser("secretcode"));
 
 app.use(sessions({
   secret: process.env.SESSION_SECRET,
@@ -161,6 +161,6 @@ app.get('/logout',(req,res) => {
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
