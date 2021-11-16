cd527a93031209f337f87132a6ea25fc56accd6b
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-16 16:47:07 +0700

    1

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(cookieParser("secret"));
+app.use(cookieParser());
 //axios
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
